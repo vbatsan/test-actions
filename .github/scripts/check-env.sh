8a179912735ac4a9c82616f3e163e2944ecefad8
volodymyr.batsan <vbatsan@gmail.com>
2024-01-28 10:50:53 +0200

    zx

--- .github/scripts/check-env.sh
+++ .github/scripts/check-env.sh
@@ -4,3 +4,9 @@ HIDDEN_SERVICE="Test-service"
 RELEASED_SERVICE="Release service"
 
 echo "HIDDEN_SERVICE=${HIDDEN_SERVICE}" >> $GITHUB_ENV
+
+# Print the environment variable to verify it's set correctly
+echo "HIDDEN_SERVICE is set to: $HIDDEN_SERVICE"
+
+# You can also export the variable for use in subsequent steps
+export HIDDEN_SERVICE
