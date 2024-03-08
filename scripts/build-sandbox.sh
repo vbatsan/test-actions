22f70861e0802c38da0a549a628408eac831d39f
Volodymyr Batsan <vbatsan@gmail.com>
2024-03-08 06:50:55 +0200

    test-1

--- scripts/build-sandbox.sh
+++ scripts/build-sandbox.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-git checkout -b sandbox
-touch text.txt
+# Define the branch name
+branch_name="sandbox"
+
+# Check if the branch exists
+if git show-ref --verify --quiet "refs/heads/$branch_name"; then
+  echo "Branch $branch_name already exists"
+  git checkout $branch_name
+else
+  # Create the branch if it doesn't exist
+  git checkout -b "$branch_name"
+  echo "Created and checked out new branch: $branch_name"
+fi
+
 git add .
-git push origin sandbox
+git commit -m "build sandbox"
+git push origin $branch_name
 git checkout -
+
+echo "Successfully push to sandbox"
